aa0913f67e82200788913918082db38684871fed
Carlos Alberto <carlin.alberto@gmail.com>
2017-10-27 18:35:44 -0200

    Download script

---
+++ download.js
@@ -0,0 +1,40 @@
+const AWS = require('aws-sdk')
+const fs = require('fs-extra')
+const moment = require('moment')
+const config = require('config')
+const aws = config.aws
+
+AWS.config = new AWS.Config(aws)
+
+const s3 = new AWS.S3()
+const today = moment().format('YYYY-MM-DD')
+
+const params = { 
+  Bucket: config.tamarac_bucket,
+  Delimiter: '/',
+  Prefix: `${today}/`
+}
+
+s3.listObjects(params, (err, data) => {
+  if (err) {
+    throw err
+  }
+  
+  data.Contents.forEach(content => {
+    const fileParams = { Bucket: config.tamarac_bucket, Key: content.Key }
+    const path = `./scripts/${content.Key}`
+
+    fs.ensureFile(path, err => {
+      if (err) {
+        throw err
+      }
+
+      const file = fs.createWriteStream(path)
+
+      s3.getObject(fileParams)
+        .on('httpData', chunk => file.write(chunk))
+        .on('httpDone', () => file.end())
+        .send()
+    })
+  })
+})
